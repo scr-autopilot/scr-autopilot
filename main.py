325ee4d58b1f28a847dd149621e957c671a7592d
MaTY-MT <62469448+MaTY-MT@users.noreply.github.com>
2021-07-04 19:04:08 +0200

    Dialog boxes
    - Most outputs and inputs are made by using dialog boxes
    - When an error happens, ask to join the support server

--- main.py
+++ main.py
@@ -10,14 +10,33 @@ from PIL import ImageFilter
 import sched
 import sys
 import tkinter
-from tkinter import ttk
+from tkinter import ttk, simpledialog, messagebox
 from throttle import *
 import requests
 import logging
 from win32 import win32api
 import ctypes
 from ahk import AHK
+import webbrowser
 ahk = AHK()
+
+root = tkinter.Tk()
+photo = tkinter.PhotoImage(file="img/ap_icon.png")
+root.title("button")
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+w = 50
+h = 50
+x = 100
+y = 100
+root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+root.lift()
+root.overrideredirect(True)
+root.call('wm', 'attributes', '.', '-topmost', '1')
+button = tkinter.Button(root, text="button1",
+                        image=photo, bg="orange")
+button.grid(column=1, row=1, sticky=tkinter.E+tkinter.W)
+root.grid_columnconfigure(2, weight=2)
 
 print("SCR-Autopilot v0.3.1-beta by MaTY (matyroblox01)")
 print("Checking for updates...")
@@ -26,16 +45,25 @@ r = requests.get(url=URL)
 data = r.json()
 version = data['version']
 if not version == "0.3.1":
-    print("Your version is outdated! Please install the latest release on https://github.com/MaTY-MT/scr-autopilot/releases")
+    print("Your version is outdated! Please install the latest release on https://github.com/scr-autopilot/scr-autopilot/releases")
+    outdatedask = messagebox.askyesno(
+        "SCR-Autopilot", "Your version of SCR-Autopilot is outdated. Do you want to go to the releases page to download a new version?")
+    if outdatedask == True:
+        webbrowser.open(
+            "https://github.com/scr-autopilot/scr-autopilot/releases")
+        exit()
 else:
     print("Your version is up-to-date.")
 logging.basicConfig(filename='autopilot.log', filemode='w',
                     level=logging.DEBUG, format="[%(levelname)s] [%(asctime)s] %(message)s")
 
-input("By using this software, you agree, that if the software makes a fault, you are always ready to take over. We are not responsible for any penalties given to your account! It is highly reccomended to use this software only on VIP servers, yet. This software is not an exploit (confirmed by the SCR staff team) and you can use it freely. Press ENTER to continue.")
+warningask = messagebox.askokcancel("Warning", "By using this software, you agree that if the software makes a fault, you are always ready to take over. We are not responsible for any penalties given to your account! It is highly recommended to use this software only on VIP servers. This software is not an exploit (confirmed by the SCR staff team) and you can use it freely.")
+if warningask == False:
+    exit()
 display_size = ImageGrab.grab().size
 logging.debug(f'Display resolution: {display_size[0]}, {display_size[1]}')
-resolution = input("What is the resolution? (fhd, hd) > ")
+resolution = simpledialog.askstring(
+    "Question", "What is the resolution? (fhd, hd)")
 if resolution == "fhd":
     spd_pos = 884, 957, 947, 985
     lim_pos = 889, 987, 942, 1016
@@ -44,11 +72,11 @@ if resolution == "fhd":
     double_yellow_pos = 1438, 950, 1439, 951
     red_pos = 1438, 1045, 1439, 1046
     distance_pos = 555, 1046, 605, 1070
-    awsbutton_pos = 1330,994,1331,995
+    awsbutton_pos = 1330, 994, 1331, 995
     throttle_pos = 843, 931, 845, 1074
     doors_pos = 870, 822, 871, 823
     loading_pos = 781, 823, 782, 824
-    continue_pos = 1032,460,1033,461
+    continue_pos = 1032, 460, 1033, 461
 elif resolution == "hd":
     print("The autopilot can be a little more buggy because of the HD resolution.")
     time.sleep(1)
@@ -62,16 +90,18 @@ elif resolution == "hd":
     awsbutton_pos = 1047, 597, 1048, 598
     throttle_pos = 522, 570, 525, 713
 else:
-    print('Hmm, the resolution is not right... Try it again. Please type only "fhd" (without the quotation marks) if you have FHD monitor, or type "hd" (without the quotation marks) if you have HD monitor.')
-    input("Press ENTER to close the program.")
+    messagebox.showerror(
+        "Error", 'Please type only "fhd" (without the quotation marks) if you have FHD monitor, or type "hd" (without the quotation marks) if you have HD monitor.')
     sys.exit()
 
 
-max_speed = int(input(
-    "What is the maximum speed of your train in MPH? (E.g. 100, 125, 75 etc.) > "))
-continue_route = int(input(
-    "Would you like to automatically continue in the route after finsihing? (0 - no, 1 - yes) > "))
-
+max_speed = simpledialog.askinteger(
+    "Question", "What is the maximum speed of your train in MPH? (E.g. 100, 125, 75 etc.)", minvalue=1)
+continue_route = simpledialog.askinteger(
+    "Question", "Would you like to automatically continue in the route after finsihing? (0 - no, 1 - yes)", minvalue=0, maxvalue=1)
+if max_speed == None or continue_route == None:
+    messagebox.showerror("Error", 'Settings incorrect. Please try again.')
+    exit()
 PROCESS_PER_MONITOR_DPI_AWARE = 2
 MDT_EFFECTIVE_DPI = 0
 
@@ -110,9 +140,12 @@ print("""  ___  ___ ___      _       _            _ _     _
  |___/\___|_|_\  /_/ \_\_,_|\__\___/ .__/_|_\___/\__|
                                    |_|               
 """)
-print("Press the red button that has appeared on your screen to engage the autopilot. You can press the button again to disengage the autopilot.")
+print("Settings:")
+print("Screen resolution:", resolution)
+print("Train max speed:", max_speed)
+print("Automatically continue:", continue_route)
 
-root = tkinter.Tk()
+
 def task():
     global solve
     global continuing
@@ -120,18 +153,17 @@ def task():
         im = ImageGrab.grab(bbox=(continue_pos))
         pix = im.load()
         continue_value = pix[0, 0]  # Set the RGBA Value of the image (tuple)
-        print(continue_value)
-        if continue_value == (255,255,255):
+        if continue_value == (255, 255, 255):
             ahk.click(991, 470)
             ahk.click(327, 833)
             continuing = True
     im = ImageGrab.grab(bbox=(awsbutton_pos))
     pix = im.load()
     awsbutton_value = pix[0, 0]  # Set the RGBA Value of the image (tuple)
-    if awsbutton_value == (255,255,255):
+    if awsbutton_value == (255, 255, 255):
         pydirectinput.keyDown("q")
         pydirectinput.keyUp("q")
-        print("AWSBUTTON:", "clicked")
+        print("Reset the AWS")
     logging.debug(f'AWS pixel RGB: {awsbutton_value}')
     cap = ImageGrab.grab(bbox=(throttle_pos))
     img = cap
@@ -152,10 +184,14 @@ def task():
         logging.debug(f'Throttle pixel RGB: {bottom_throttle_pixel}')
     print("Current throttle: ", currentThrottle)
 
-    #print(count,"pixels are green.")
     if currentThrottle == None:
-        print("I can't read the throttle.")
-
+        messagebox.showerror("Error", "I can't read the throttle")
+        supportask = messagebox.askyesno(
+            "Question", "It looks like you got an error. You can try again, but if this error persists, you can join the support server. Do you want to join the support server on Discord?")
+        if supportask == True:
+            webbrowser.open(
+                "https://discord.gg/jtQ2R8cxWq")
+            exit()
     else:
         # LIMIT
         cap = ImageGrab.grab(bbox=(lim_pos))
@@ -167,7 +203,13 @@ def task():
         lim = 0
         lim = [int(s) for s in re.findall(r'\b\d+\b', tesstr)]
         if lim == []:
-            print("I can't read the limit.")
+            messagebox.showerror("Error", "I can't read the limit")
+            supportask = messagebox.askyesno(
+                "Question", "It looks like you got an error. You can try again, but if this error persists, you can join the support server. Do you want to join the support server on Discord?")
+            if supportask == True:
+                webbrowser.open(
+                    "https://discord.gg/jtQ2R8cxWq")
+                exit()
         else:
             templim = lim[0]
             lim = lim[0]
@@ -265,20 +307,9 @@ def onClick():
         root.after_cancel(solve)
 
 
-photo = tkinter.PhotoImage(file="img/ap_icon.png")
-root.title("button")
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-w = 50
-h = 50
-x = 100
-y = 100
-root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-root.lift()
-root.overrideredirect(True)
-root.call('wm', 'attributes', '.', '-topmost', '1')
-button = tkinter.Button(root, text="button1",
-                        image=photo, bg="red", command=onClick)
-button.grid(column=1, row=1, sticky=tkinter.E+tkinter.W)
-root.grid_columnconfigure(2, weight=2)
+button.configure(bg="red", command=onClick)
+switchask = messagebox.askyesno(
+    "SCR-Autopilot", 'Autopilot is set up. Do you want to turn it on now? You can also turn it on or off by using the "AP" button on your screen.')
+if switchask == True:
+    onClick()
 root.mainloop()
